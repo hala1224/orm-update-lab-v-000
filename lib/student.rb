b6c6dc9c485913bd518c3c6bed174c3c054605cf
Natalie Halawani <hala1224@gmail.com>
2019-02-24 00:48:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,5 +12,16 @@ class Student
     @grade=grade
   end
   
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
 
 end
